b5db398ff0a455567616591ebd896f94c796f362
vinci <vincilee1994@outlook.com>
2022-12-11 21:10:57 +0800

    update

--- JD/purchase.js
+++ JD/purchase.js
@@ -43,6 +43,7 @@ async function waitForReserveStateDone(page)
         if (count > 0) {
             console.log("reload page")
             await page.goto('https://cart.jd.com/cart_index');
+            await waitForTimeout(250)
         } else {
             return
         }
@@ -52,6 +53,7 @@ async function waitForReserveStateDone(page)
 async function cleanCart(page, targetTime)
 {
     await page.goto('https://cart.jd.com/cart_index');
+    await page.waitForURL('https://cart.jd.com/cart_index');
     await executeAt(async () => {
         await page.goto('https://cart.jd.com/cart_index');
     }, targetTime);
@@ -67,6 +69,7 @@ async function cleanCart(page, targetTime)
         do {
             state = await page.locator('#checkedCartState').getAttribute('value');
             console.log("state " + state);
+            await waitForTimeout(50)
         } while (state != "1")
     })
     await page.getByText('去结算').click();
@@ -88,7 +91,13 @@ async function buy(page, url)
     const browser = await chromium.launch({
         headless: false
     });
-    const context = await browser.newContext({storageState: __dirname+'/Auth.json'});
+    const context = await browser.newContext({
+        storageState: __dirname+'/Auth.json',
+        recordVideo: {
+            dir: "./recordings"
+        }
+    });
+    const context1 = await browser.newContext();
     const page = await context.newPage();
     await cleanCart(page, new Date(Date.parse('2022-12-11T20:00:00.010')))
     
